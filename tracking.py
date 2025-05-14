4005c3ea31023cb6c7e2c291a3e0ea3ce780ca1a
Estella <essrasava@gmail.com>
2025-05-14 13:05:37 +0300

    edited

--- tracking.py
+++ tracking.py
@@ -28,7 +28,7 @@ def get_detections_in_roi(results, polygon_pts):
             conf = box.conf[0].item()
             cls = int(box.cls[0].item())
 
-            if cls != 2:  # Only cars
+            if cls != 0:  # Only cars
                 continue
 
             # Calculate center of the bounding box
@@ -43,16 +43,4 @@ def get_detections_in_roi(results, polygon_pts):
             detections.append(([x_min, y_min, x_max - x_min, y_max - y_min], conf, 'car'))
     return detections
 
-# function transforms the pixel coordinates to real-world coordinates using the perspective transformation matrix.
-def transform_point(cx, cy, matrix):
-    point = np.array([[[cx, cy]]], dtype=np.float32)
-    return cv2.perspectiveTransform(point, matrix)[0][0]
 
-# Calculate moving average position of the detected object
-# function takes a history of positions and returns the average position over the last 'smooth_size' frames.
-def moving_average_position(position_history, smooth_size=10):
-    # If less frames are accumulated than required for the window, we use the existing ones
-    window = list(position_history)[-smooth_size:] if len(position_history) >= smooth_size else list(position_history)
-    avg_x = sum(pos[0] for pos in window) / len(window)
-    avg_y = sum(pos[1] for pos in window) / len(window)
-    return avg_x, avg_y
